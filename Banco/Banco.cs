206099fd8b176bd4403303d6e6155cd4d9a8eca7
MatheusPRodrigues <matherodrigues17@gmail.com>
2025-10-16 08:08:29 -0300

    salvando

--- Banco/Banco.cs
+++ Banco/Banco.cs
@@ -10,8 +10,31 @@ namespace Banco
     {
         public Fila FilaNormal { get; set; }
         public Fila FilaPrioritaria { get; set; }
+        public int Contador { get; set; }
 
+        public Banco()
+        {
+            this.FilaNormal = new Fila();
+            this.FilaPrioritaria = new Fila();
+            this.Contador = 0;
+        }
 
+        public Pessoa AtenderCliente()
+        {
+            if (this.Contador > 2)
+                this.Contador = 0;
+
+            if (this.Contador == 0)
+            {
+                this.Contador++;
+                return this.FilaPrioritaria.Desenfileirar();
+            }
+            else
+            {
+                this.Contador++;
+                return this.FilaNormal.Desenfileirar();
+            }
+        }
 
     }
 }
--- Banco/Program.cs
+++ Banco/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//Simulação de Atendimento em um Banco
+
+
+using BancoAtendimento;
+
+void MenuPrincipal()
+{
+    Banco banco = new Banco();
+
+    bool repetir = true;
+}
